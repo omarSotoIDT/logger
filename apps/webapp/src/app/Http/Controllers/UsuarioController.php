51f767932af18ebabe439b1e2d74c0ba61d9ec88
Manuel <developer@developer-Vostro-5415>
2025-12-23 18:12:01 -0600

    Actualizar UsuarioController.php y UsuarioRH.php

--- apps/webapp/src/app/Http/Controllers/UsuarioController.php
+++ apps/webapp/src/app/Http/Controllers/UsuarioController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\const\StatusConsts;
+use App\Services\UsuarioService;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 class UsuarioController
 {
-    public function index() {
-        return view('usuarios.index');
+    public function gestor() {
+        try{
+            $usuarios = UsuarioService::listarUsuarios([],['status' => StatusConsts::ACTIVO], null, null, '');
+
+            return view('usuarios.index', compact('usuarios'));
+        }catch(Exception $e){
+            return back()->with('error', "Error al listar los usuarios");
+        }
+    }
+
+    public function agregar(Request $request)
+    {
+        try{
+            $validar = Validator::make($request->all(), [
+
+            ]);
+        }catch(Exception $e){
+            return back()->with('error', 'No se agrego el usuario correctamente');
+        }
     }
 }
---
+++ apps/webapp/src/app/Repositories/RH/UsuarioRH.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Repositories\RH;
+
+class UsuarioRH
+{
+    public static function obtenerColumnas($columnas, &$query){
+
+        $camposUsuarios = [
+            'usuariosId' => 'usuario_id',
+            'usuario' => 'usuario',
+            'nombreCorto' => 'nombre_corto',
+            'email' => 'email',
+            'telefono' => 'telefono',
+            'ultimoAccesoFecha' => 'ultimo_acceso_fecha',
+            'status' => 'status',
+            'registroFecha' => 'registro_fecha',
+            'registroAutorId' => 'registro_autor_id',
+            'actualizacionFecha' => 'actualizacion_fecha',
+            'actualizacionAutorId' => 'actualizacion_autor_id'
+        ];
+
+        if(empty($columnas)){
+            foreach($camposUsuarios as $value){
+                $query->addSelect($value);
+            }
+        }else{
+            foreach($camposUsuarios as $value){
+                if(isset($camposUsuarios[$value])){
+                    $query->addSelect($camposUsuarios[$value]);
+                }
+            }
+        }
+    }
+
+    public static function obtenerFiltro($filtro, &$query)
+    {
+        $filtros_usuarios = [
+            'usuario' => 'usuario',
+            'status' => 'status'
+        ];
+
+        foreach($filtro as $key => $value){
+            if(!empty($filtros_usuarios[$value])){
+                if(is_array($filtro)){
+                    $query->whereIn($key, $filtros_usuarios[$value]);
+                }else{
+                    $query->where($key, $filtros_usuarios[$value]);
+                }
+            }
+        }
+    }
+
+    public static function obtenerOrden($orden, &$query){
+
+        $ordersDisponibles = [
+            'usuario_asc'   => ['usuario', 'asc'],
+            'usuario_desc'  => ['usuario', 'desc'],
+
+            'status_asc'          => ['status', 'asc'],
+            'status_desc'         => ['status', 'desc'],
+
+            'registro_fecha_asc'  => ['registro_fecha', 'asc'],
+            'registro_fecha_desc' => ['registro_fecha', 'desc'],
+        ];
+
+        $defaultKey = 'usuario_asc';
+
+        $key = !empty($orden) ? $orden : $defaultKey;
+
+        if (!isset($ordersDisponibles[$key])) {
+            $key = $defaultKey;
+        }
+
+        [$columna, $direccion] = $ordersDisponibles[$key];
+
+        $query->orderBy($columna, $direccion);
+    }
+}
