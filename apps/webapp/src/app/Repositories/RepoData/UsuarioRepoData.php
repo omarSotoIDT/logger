9705c6b6d4de1c84179cc4e04e95f6f1fc664eac
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2025-12-30 12:11:56 -0600

    feat(Usuarios): metodo listar y obtener

---
+++ apps/webapp/src/app/Repositories/RepoData/UsuarioRepoData.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories\RepoData;
+
+use App\Repositories\RH\UsuarioRH;
+use Illuminate\Support\Facades\DB;
+
+class UsuarioRepoData{
+
+    public static function listar($columnas, $filtros, $limit, $offset, $orden){
+        $query = DB::table('sys_usuarios');
+
+        UsuarioRH::obtenerColumnas($columnas, $query);
+        UsuarioRH::obtenerFiltro($filtros, $query);
+        UsuarioRH::obtenerOrden($orden,$query);
+
+        if(!empty($limit)){
+            $query->limit($limit);
+        }
+
+        if(!empty($offset)){
+            $query->offset($offset);
+        }
+        
+        return $query->get();
+    }
+
+    public static function obtener($id, $columnas, $filtros)
+    {
+        $query = DB::table('sys_usuarios');
+
+        UsuarioRH::obtenerColumnas($columnas, $query);
+        UsuarioRH::obtenerFiltro($filtros, $query);
+
+        return $query->where('usuario_id', $id)->first();
+    }
+}
