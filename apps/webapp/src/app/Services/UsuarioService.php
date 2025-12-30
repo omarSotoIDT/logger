79d8401dcc2d96e43716fde46f41aeba842f10cd
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2025-12-30 12:26:32 -0600

    feat(Usuarios): Agregar metodos para crud de usuario

---
+++ apps/webapp/src/app/Services/UsuarioService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\BO\UsuariosBO;
+use App\const\StatusConsts;
+use App\Repositories\RepoAction\UsuarioRepoAction;
+use App\Repositories\RepoData\UsuarioRepoData;
+
+class UsuarioService{
+
+    public static function listarUsuarios($columna, $filtros, $limite, $offset, $orden){
+        return UsuarioRepoData::listar($columna, $filtros, $limite, $offset, $orden);
+    }
+
+    public static function obtenerUsuario($id, $columna, $filtros){
+        return UsuarioRepoData::obtener($id, $columna, $filtros);
+    }
+
+    public static function agregarUsuario($datos)
+    {
+        $insertarUsuario = UsuariosBO::agregar($datos);
+
+        return UsuarioRepoAction::agregar($insertarUsuario);
+    }
+
+    public static function editarUsuario($datos, $id){
+
+        $actualizarDatos = UsuariosBO::editar($datos);
+
+        return UsuarioRepoAction::editar($actualizarDatos, $id);
+    }
+
+    public static function eliminarUsuario($id)
+    {
+        $status = StatusConsts::ELIMINADO;
+
+        $eliminarUsuario = UsuariosBO::eliminar($status);
+
+        return UsuarioRepoAction::editar($eliminarUsuario, $id);
+    }
+}
