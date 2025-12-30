db86c17a86151aea66b2af8fba32576e7622550b
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2025-12-30 12:45:38 -0600

    feat(Usuarios): Agregar metodos

---
+++ apps/webapp/src/app/BO/UsuariosBO.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\BO;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UsuariosBO{
+    public static function agregar($datos){
+        return [ 'usuario' => $datos['usuario'],
+            'email' => $datos['email'],
+            'password' => Hash::make($datos['password']),
+            'nombre_corto' => $datos['nombreCorto'],
+
+            'registro_fecha' => now(),
+            'registro_autor_id' => Auth::id()
+        ];
+    }
+
+    public static function editar($datos)
+    {
+        return [
+            'usuario' => $datos['usuario'],
+            'email' => $datos['email'],
+            'password' => Hash::make($datos['password']),
+            'nombre_corto' => $datos['nombreCorto'],
+
+            'actualizacion_fecha' => now(),
+            'actualizacion_autor_id' => Auth::id()
+        ];
+    }
+
+    public static function eliminar($status){
+        return [
+            'status' => $status,
+
+            'actualizacion_fecha' => now(),
+            'actualizacion_autor_id' => Auth::id()
+        ];
+    }
+}
