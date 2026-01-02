3205428688aae80ac2e473debfa6f20ef4014d6c
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2026-01-02 11:35:10 -0600

    feat(Usuarios): Agregar migraciones

---
+++ apps/webapp/src/database/migrations/2025_12_30_133705_create_rel_usuarios_proyectos.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rel_usuarios_proyectos', function (Blueprint $table) {
+            $table->bigIncrements('rel_usuario_proyecto_id');
+
+            $table->unsignedBigInteger('usuario_id');
+            $table->unsignedBigInteger('proyecto_id');
+            $table->string('status', 20)->default('INACTIVO');
+
+             // Auditoría
+            $table->timestamp('registro_fecha');
+            $table->unsignedBigInteger('registro_autor_id');
+            $table->timestamp('actualizacion_fecha')->nullable();
+            $table->unsignedBigInteger('actualizacion_autor_id')->nullable();
+
+            $table->foreign('usuario_id')
+            ->references('usuario_id')
+            ->on('sys_usuarios')
+            ->onDelete('cascade');
+
+            $table->foreign('proyecto_id')
+            ->references('proyecto_id')
+            ->on('proyectos')
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rel_usuarios_proyectos');
+    }
+};
