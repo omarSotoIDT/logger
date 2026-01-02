a467e29a9a30c209ee188b9b04617f5119314fc1
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2026-01-02 13:49:46 -0600

    feat(Usuarios): Agregar metodo eliminar

--- apps/webapp/src/app/Http/Controllers/UsuarioController.php
+++ apps/webapp/src/app/Http/Controllers/UsuarioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\const\StatusConsts;
+use App\Coordinators\UsuarioCoordinator;
 use App\Services\UsuarioService;
 use Exception;
 use Illuminate\Http\Request;
@@ -12,9 +13,9 @@ class UsuarioController
 {
     public function gestor() {
         try{
-            $usuarios = UsuarioService::listarUsuarios([],['status' => StatusConsts::ACTIVO], null, null, '');
+            [$usuarios, $proyectos] = UsuarioCoordinator::listarProyectosyUsuarios();
 
-            return view('usuarios.index', compact('usuarios'));
+            return view('usuarios.index', compact('usuarios', 'proyectos'));
         }catch(Exception $e){
             return back()->with('error', "Error al listar los usuarios");
         }
@@ -24,10 +25,34 @@ class UsuarioController
     {
         try{
             $validar = Validator::make($request->all(), [
-
+                'usuario' => 'required',
+                'email' => 'required',
+                'nombreCorto' => 'required',
+                'password' => 'required',
             ]);
+
+            if($validar->fails()) {
+                return back()->withErrors($validar)->withInput();
+            }
+
+            $data = $validar->validated();
+
+            UsuarioService::agregarUsuario($data);
+
+            return back()->with('success', 'El usuario se creo correctamente');
+
         }catch(Exception $e){
             return back()->with('error', 'No se agrego el usuario correctamente');
+        }
+    }
+
+    public function eliminar($id) {
+        try {
+            UsuarioService::eliminarUsuario($id);
+
+            return back()->with('success', 'Usuario eliminado correctamente');
+        } catch(Exception $e) {
+            return back()->with('error', 'Error al eliminar el Usuario');
         }
     }
 }
