5300c97a1c84277d553f102029c512338592be5a
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2026-01-02 14:25:34 -0600

    feat(Usuarios): Agregar metodo editar

--- apps/webapp/src/app/Http/Controllers/UsuarioController.php
+++ apps/webapp/src/app/Http/Controllers/UsuarioController.php
@@ -28,7 +28,7 @@ class UsuarioController
                 'usuario' => 'required',
                 'email' => 'required',
                 'nombreCorto' => 'required',
-                'password' => 'required',
+                'password' => 'required|min:8',
             ]);
 
             if($validar->fails()) {
@@ -43,6 +43,30 @@ class UsuarioController
 
         }catch(Exception $e){
             return back()->with('error', 'No se agrego el usuario correctamente');
+        }
+    }
+
+    public function actualizar(Request $request, $id) {
+        try {
+            $validator = Validator::make($request->all(), [
+                'usuario' => 'required',
+                'email' => 'required',
+                'nombreCorto' => 'required',
+                'password' => 'nullable|min:8'
+            ]);
+
+            if($validator->fails()) {
+                return back()->withErrors($validator)->withInput();
+            }
+
+            $data = $validator->validated();
+
+            UsuarioService::editarUsuario($data, $id);
+
+            return back()->with('success', 'Usuario actualizado correctamente');
+
+        } catch (Exception $e) {
+            return back()->with('error', 'Error al actualizar el usuario');
         }
     }
 
