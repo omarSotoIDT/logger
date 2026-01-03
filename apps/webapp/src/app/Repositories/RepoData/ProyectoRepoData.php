1eaebec1680140660f025382c872e4cec17a60d4
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2026-01-03 12:19:14 -0600

    feat(Usuarios): metodo para consultar proyectos por usuario

--- apps/webapp/src/app/Repositories/RepoData/ProyectoRepoData.php
+++ apps/webapp/src/app/Repositories/RepoData/ProyectoRepoData.php
@@ -27,7 +27,27 @@ class ProyectoRepoData
         return $query->get();
     }
 
-    public static function obtener($id, $columnas)
+    public static function listarProyectosPorUsuario($columnas, $filtros,$limit, $offset, $orden){
+        $query = DB::table('rel_usuarios_proyectos AS rup')
+            ->join('proyectos AS p', 'p.proyecto_id', '=', 'rup.proyecto_id')
+            ->select('p.*');
+    
+        ProyectoRH::obtenerColumnas($query, $columnas);
+        ProyectoRH::obtenerFiltros($query, $filtros);
+        ProyectoRH::obtenerOrden($query, $orden);
+    
+        if(!empty($offset)) {
+            $query->offset($offset);
+        }
+
+        if(!empty($limit)) {
+            $query->limit($limit);
+        }
+    
+        return $query->get();
+    }
+
+    public static function obtener($id, $filtros, $columnas)
     {
         $query = DB::table('proyectos AS p')
             ->leftJoin('cat_tipos_proyecto AS ctp', 'ctp.tipo_proyecto_id', '=', 'p.tipo_proyecto_id');
