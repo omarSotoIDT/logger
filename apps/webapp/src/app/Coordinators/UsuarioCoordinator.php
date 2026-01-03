7e28756e5d21acad42627659d08b013c530f2aac
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2026-01-03 12:35:38 -0600

    feat(Usuarios): asociar perfiles y proyectos a cada usuario

--- apps/webapp/src/app/Coordinators/UsuarioCoordinator.php
+++ apps/webapp/src/app/Coordinators/UsuarioCoordinator.php
@@ -3,6 +3,7 @@
 namespace App\Coordinators;
 
 use App\const\StatusConsts;
+use App\Services\PerfilService;
 use App\Services\ProyectoService;
 use App\Services\UsuarioService;
 
@@ -15,6 +16,17 @@ class UsuarioCoordinator
         $proyectos = ProyectoService::listarProyectos([
             'status' => [StatusConsts::ACTIVO]
         ]);
+
+        $proyectoPorUsuario = ProyectoService::listarProyectosPorUsuario('p.nombre,p.proyecto_id,rup.usuario_id,p.status',
+        ['p.status' => StatusConsts::ACTIVO],null,null,'usuario_id_asc')->groupBy('usuario_id');
+
+        $perfilPorUsuario = PerfilService::listarPerfilesPorUsuarios('up.usuario_id,pf.perfil_id,pf.nombre',
+        ['pf.status' => StatusConsts::ACTIVO], null, null, 'usuario_id_asc')->groupBy('usuario_id');
+
+        foreach ($usuarios as $usuario) {
+            $usuario->proyectos = $proyectoPorUsuario[$usuario->usuario_id] ?? collect();
+            $usuario->perfiles  = $perfilPorUsuario[$usuario->usuario_id] ?? collect();
+        }
 
         return [$usuarios, $proyectos];
     }
