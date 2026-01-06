66ec8135ab830b38c78af11c45641d53c1a5bd42
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2026-01-06 17:59:05 -0600

    fix(usuarios): parametros con valores por defecto

--- apps/webapp/src/app/Services/ProyectoService.php
+++ apps/webapp/src/app/Services/ProyectoService.php
@@ -3,16 +3,24 @@
 namespace App\Services;
 
 use App\BO\ProyectoBO;
+use App\const\StatusConsts;
 use App\Repositories\RepoAction\ProyectoRepoAction;
 use App\Repositories\RepoData\ProyectoRepoData;
 
 class ProyectoService
 {
-    public static function listarProyectos($filtros = [], $columnas = '', $limit = null, $offset = null, $orden = '') {
+    public static function listarProyectos($filtros = [], $columnas = '', $limit = null, $offset = null, $orden = '')
+    {
         return ProyectoRepoData::listar($filtros, $columnas, $limit, $offset, $orden);
     }
 
-    public static function listarProyectosPorUsuario($columna, $filtros, $limite, $offset, $orden){
+    public static function listarProyectosPorUsuario(
+        $columna =  'p.nombre,p.proyecto_id,rup.usuario_id,p.status',
+        $filtros =  ['p.status' => StatusConsts::ACTIVO],
+        $limite = null,
+        $offset = null,
+        $orden = 'registro_fecha_asc'
+    ) {
         return ProyectoRepoData::listarProyectosPorUsuario($columna, $filtros, $limite, $offset, $orden);
     }
 
