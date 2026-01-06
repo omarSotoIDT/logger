723d2c5e4e39ae507f699cbc96cd4aa1f09b088b
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2026-01-06 09:59:48 -0600

    fix(Usuarios): agregar parametro

--- apps/webapp/src/app/Services/ProyectoService.php
+++ apps/webapp/src/app/Services/ProyectoService.php
@@ -18,7 +18,7 @@ class ProyectoService
 
     public static function obtenerProyecto($id, $filtros = [], $columnas = '')
     {
-        return ProyectoRepoData::obtener($id, $columnas);
+        return ProyectoRepoData::obtener($id, $filtros, $columnas);
     }
 
     public static function agregarProyecto($data)
