a869a677e6bf2405e7c17d7ad87382190056cf0c
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2026-01-06 18:05:22 -0600

    fix(usarios): agregar transaction

--- apps/webapp/src/app/Coordinators/UsuarioCoordinator.php
+++ apps/webapp/src/app/Coordinators/UsuarioCoordinator.php
@@ -6,22 +6,21 @@ use App\const\StatusConsts;
 use App\Services\PerfilService;
 use App\Services\ProyectoService;
 use App\Services\UsuarioService;
+use Illuminate\Support\Facades\DB;
 
 class UsuarioCoordinator
 {
     public static function listarProyectosyUsuarios()
     {
-        $usuarios = UsuarioService::listarUsuarios([],['status' => StatusConsts::ACTIVO], null, null, null);
+        $usuarios = UsuarioService::listarUsuarios();
 
         $proyectos = ProyectoService::listarProyectos([
             'status' => [StatusConsts::ACTIVO]
         ]);
 
-        $proyectoPorUsuario = ProyectoService::listarProyectosPorUsuario('p.nombre,p.proyecto_id,rup.usuario_id,p.status',
-        ['p.status' => StatusConsts::ACTIVO],null,null,'usuario_id_asc')->groupBy('usuario_id');
+        $proyectoPorUsuario = ProyectoService::listarProyectosPorUsuario()->groupBy('usuario_id');
 
-        $perfilPorUsuario = PerfilService::listarPerfilesPorUsuarios('up.usuario_id,pf.perfil_id,pf.nombre',
-        ['pf.status' => StatusConsts::ACTIVO], null, null, 'usuario_id_asc')->groupBy('usuario_id');
+        $perfilPorUsuario = PerfilService::listarPerfilesPorUsuarios()->groupBy('usuario_id');
 
         foreach ($usuarios as $usuario) {
             $usuario->proyectos = $proyectoPorUsuario[$usuario->usuario_id] ?? collect();
@@ -33,17 +32,21 @@ class UsuarioCoordinator
 
     public static function agregarUsuarioConProyecto($data)
     {
-        $usuarioId = UsuarioService::agregarUsuario($data);
+        DB::transaction(function () use ($data) {
+            $usuarioId = UsuarioService::agregarUsuario($data);
 
-        foreach($data['proyectos'] ?? [] as $proyectoId){
-            UsuarioService::agregarRelacionProyecto($usuarioId, $proyectoId);
-        }
+            foreach ($data['proyectos'] ?? [] as $proyectoId) {
+                UsuarioService::agregarRelacionProyecto($usuarioId, $proyectoId);
+            }
+        }, attempts: 2);
     }
 
     public static function actualizarUsuarioConProyecto($data, $usuarioId)
     {
-        UsuarioService::editarUsuario($data, $usuarioId);
+        DB::transaction(function () use ($data, $usuarioId) {
+            UsuarioService::editarUsuario($data, $usuarioId);
 
-        UsuarioService::nuevaRelacionProyecto($usuarioId, $data['proyectos'] ?? []);
+            UsuarioService::nuevaRelacionProyecto($usuarioId, $data['proyectos'] ?? []);
+        }, attempts: 2);
     }
 }
