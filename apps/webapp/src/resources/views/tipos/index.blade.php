9dcb317eac4d9530c60c2df16065ecc91d0c8303
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2026-01-07 17:07:45 -0600

    fix(usuarios): se agrego un icono svg en cada vista

--- apps/webapp/src/resources/views/tipos/index.blade.php
+++ apps/webapp/src/resources/views/tipos/index.blade.php
@@ -23,8 +23,9 @@
                         type="button"
                         class="modal-cerrar"
                         aria-label="Cerrar"
-                        @click.prevent="cerrarModal('crear')"
-                    >✕</button>
+                        @click.prevent="cerrarModal('crear')">
+                        <img src="{{ asset('assets/icons/x.svg') }}" alt="icono">
+                    </button>
                 </header>
 
                 <form class="modal-cuerpo" method="POST" action="{{ route('tipos.crear') }}">
@@ -75,8 +76,9 @@
                         type="button"
                         class="modal-cerrar"
                         aria-label="Cerrar"
-                        @click.prevent="cerrarModal('editar')"
-                    >✕</button>
+                        @click.prevent="cerrarModal('editar')">
+                        <img src="{{ asset('assets/icons/x.svg') }}" alt="icono">
+                    </button>
                 </header>
 
                 <form class="modal-cuerpo" method="POST" id="form-editar-tipo" :action="formEditarAction">
