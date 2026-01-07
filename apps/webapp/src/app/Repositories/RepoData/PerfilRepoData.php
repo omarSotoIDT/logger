7977008eddf1a8cc8bd6dadd5a699cda17c7750a
manuelChazariIDT <manuel.chazari@idtconsultores.com>
2026-01-06 18:00:03 -0600

    fix(usuarios): eliminar select

--- apps/webapp/src/app/Repositories/RepoData/PerfilRepoData.php
+++ apps/webapp/src/app/Repositories/RepoData/PerfilRepoData.php
@@ -5,24 +5,25 @@ namespace App\Repositories\RepoData;
 use App\Repositories\RH\PerfilRH;
 use Illuminate\Support\Facades\DB;
 
-class PerfilRepoData{
-    public static function listarPerfilesPorUsuario($columnas, $filtros, $limit, $offset, $orden){
+class PerfilRepoData
+{
+    public static function listarPerfilesPorUsuario($columnas, $filtros, $limit, $offset, $orden)
+    {
         $query = DB::table('rel_usuarios_perfiles AS up')
-            ->join('sys_perfiles AS pf', 'pf.perfil_id', '=', 'up.perfil_id')
-            ->select('pf.*');
-    
+            ->join('sys_perfiles AS pf', 'pf.perfil_id', '=', 'up.perfil_id');
+
         PerfilRH::obtenerColumnas($query, $columnas);
         PerfilRH::obtenerFiltros($query, $filtros);
-        PerfilRH::obtenerOrden($query,$orden);
-    
-        if(!empty($limit)){
+        PerfilRH::obtenerOrden($query, $orden);
+
+        if (!empty($limit)) {
             $query->limit($limit);
         }
 
-        if(!empty($offset)){
+        if (!empty($offset)) {
             $query->offset($offset);
         }
-    
+
         return $query->get();
     }
 }
